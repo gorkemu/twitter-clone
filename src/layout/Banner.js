746b08d05b05cfd97c79daaac4790c95cd33cd83
gorkemu <gorkemunal88@gmail.com>
2023-02-22 02:00:33 +0300

    divide to separate components

--- src/layout/Banner.js
+++ src/layout/Banner.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AnalyticsIcon,
   AppsAndSessionsIcon,
@@ -63,37 +63,62 @@ import {
   ViewIcon,
   WorldIcon,
 } from "../assets/icons";
+import { BannerLink } from "../components/BannerLink";
 
+const bannerLinks = [
+  {
+    name: "Home",
+    icon: HomeIcon,
+  },
+  {
+    name: "Explore",
+    icon: ExploreIcon,
+  },
+  {
+    name: "Notification",
+    icon: NotificationIcon,
+  },
+  {
+    name: "Messages",
+    icon: MessagesIcon,
+  },
+  {
+    name: "Profile",
+    icon: ProfileIcon,
+  },
+  {
+    name: "More",
+    icon: MoreIcon,
+  },
+];
 const Banner = () => {
+  const [active, setActive] = useState("Home");
+
+  const handleMenuItemClick = (name) => {
+    setActive(name);
+  };
+
   return (
-    <header className="w-20 flex-col px-2">
-      <div className="p-3 w-12 h-12 my-1  text-primary-base rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-primary-light">
+    <header className="sticky top-0 w-20 h-screen flex-col px-2 justify-center">
+      <div className="p-3 w-12 h-12 my-1  text-primary-base rounded-full cursor-pointer mx-auto flex-col items-center justify-center  hover:bg-primary-lighter">
         <TwitterIcon />
       </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <HomeIcon />
-      </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <ExploreIcon />
-      </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <NotificationIcon />
-      </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <MessagesIcon />
-      </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <TwitterBlueIcon />
-      </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <ProfileIcon />
-      </div>
-      <div className="p-3 w-12 h-12 rounded-full cursor-pointer mx-auto flex items-center justify-center  hover:bg-gray-light">
-        <MoreIcon />
-      </div>
-      <div className="p-3 w-12 h-12 my-2  bg-primary-base text-white rounded-full cursor-pointer hover:bg-primary-dark mx-auto flex items-center justify-center  hover:bg-gray-light">
+      <nav className="w-12 mx-auto">
+        <ul>
+          {bannerLinks.map(({ name, icon }) => (
+            <BannerLink
+              key={name}
+              name={name}
+              Icon={icon}
+              active={active}
+              onMenuItemClick={handleMenuItemClick}
+            />
+          ))}
+        </ul>
+      </nav>
+      <button className="p-3 w-12 h-12 my-2 bg-primary-base text-white rounded-full cursor-pointer hover:bg-primary-dark mx-auto flex items-center justify-center shadow-lg transform transition-colors duration-200">
         <TweetIcon />
-      </div>
+      </button>
     </header>
   );
 };
