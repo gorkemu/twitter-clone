fa452367399e15f9523a4fa46f019a2f96b95cfe
gorkemu <gorkemunal88@gmail.com>
2023-02-22 01:57:43 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,22 @@ module.exports = {
           base: "#1d9bf0",
           dark: "#1a8cd8",
           light: "#b8e0fa",
+          lighter: "#1d9bf01a",
         },
         gray: {
           dark: "#4a4e52",
+          placeholder: "#6e7c87",
           light: "#e0e2e3",
           lighter: "#f5f5f5",
           lightest: "#f7f9f9",
+        },
+        green: {
+          dark: "#00ba7c",
+          light: "#00ba7c1a",
+        },
+        pink: {
+          dark: "#f91880",
+          light: "#f918801a",
         },
         black: "#0f1419",
       },
