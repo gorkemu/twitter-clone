d99c10e05431fa08a0fa427a59a1abd2d02ce224
gorkemu <gorkemunal88@gmail.com>
2023-02-22 20:59:51 +0300

    add BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import TimeAgo from "javascript-time-ago";
-
+import { BrowserRouter } from "react-router-dom";
 import en from "javascript-time-ago/locale/en.json";
 
 TimeAgo.addDefaultLocale(en);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
