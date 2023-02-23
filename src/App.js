bacd8ca7865ba84fdf2efd85b3d94a3ede5eebe2
gorkemu <gorkemunal88@gmail.com>
2023-02-23 22:12:01 +0300

    add login and register routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
-import Register from "./pages/Register";
+import CreateWithEmailAndPassword from "./pages/CreateWithEmailAndPassword";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/register" element={<CreateWithEmailAndPassword />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
     </Routes>
   );
 };
