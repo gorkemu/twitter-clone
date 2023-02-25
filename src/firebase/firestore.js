c7f1aa4497b03a3b5cb32d2bbf1f4ed7e3b18acf
gorkemu <gorkemunal88@gmail.com>
2023-02-25 04:44:44 +0300

    add and get tweets

---
+++ src/firebase/firestore.js
@@ -0,0 +1,53 @@
+import {
+  addDoc,
+  collection,
+  getDocs,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "./firebase";
+import { getDownloadURL } from "./storage";
+
+const TWEETS_COLLECTION = "tweets";
+
+export function addTweetWithImage(uid, content, createdAt, imageBucket) {
+  addDoc(collection(db, TWEETS_COLLECTION), {
+    uid,
+    content,
+    createdAt,
+    imageBucket,
+  });
+}
+
+export function addTweetWithoutImage(uid, content, createdAt) {
+  addDoc(collection(db, TWEETS_COLLECTION), {
+    uid,
+    content,
+    createdAt,
+  });
+}
+
+export async function getTweets(uid, setTweets) {
+  const tweetsQuery = query(
+    collection(db, TWEETS_COLLECTION),
+    where("uid", "==", uid),
+    orderBy("createdAt", "desc")
+  );
+
+  const unsubscribe = onSnapshot(tweetsQuery, async (snapshot) => {
+    let allTweets = [];
+    for (const documentSnapshot of snapshot.docs) {
+      const tweet = documentSnapshot.data();
+      allTweets.push({
+        ...tweet,
+        createdAt: tweet["createdAt"],
+        id: documentSnapshot.id,
+        imageUrl: await getDownloadURL(tweet["imageBucket"]),
+      });
+    }
+    setTweets(allTweets);
+  });
+  return unsubscribe;
+}
