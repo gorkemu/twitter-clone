f087028469867b96fe4c1ad5121c6d6ae0636c80
gorkemu <gorkemunal88@gmail.com>
2023-02-25 04:42:01 +0300

    navigate to home if logged out, prevent flash

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "../layout/Container";
 import Banner from "../layout/Banner";
 import Feed from "../layout/Feed";
 import Sidebar from "../layout/Sidebar";
+import { useAuth } from "../firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
 
 const Dashboard = () => {
-  return (
+  const { authUser, isLoading } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoading && !authUser) {
+      navigate("/");
+    }
+  }, [authUser, isLoading]);
+
+  return !authUser ? (
+    <CircularProgress sx={{ marginLeft: "50%", marginTop: "25%" }} />
+  ) : (
     <Container>
       <Banner />
       <Feed />
