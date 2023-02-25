56eab3ab09490cc7224fd05d5c5cdbdcf0707cef
gorkemu <gorkemunal88@gmail.com>
2023-02-25 04:38:54 +0300

    navigate to dashboard is user logged in

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import CreateWithEmailAndPassword from "./pages/CreateWithEmailAndPassword";
 import Login from "./pages/Login";
+import { useAuth } from "./firebase/auth";
 
 const App = () => {
+  const { authUser, isLoading } = useAuth();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoading && authUser) {
+      navigate("/dashboard");
+    }
+  }, [authUser, isLoading]);
+
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
