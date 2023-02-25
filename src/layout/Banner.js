2b3e01f65d53f2c84fbcc51af4859c43023933f3
gorkemu <gorkemunal88@gmail.com>
2023-02-25 04:43:44 +0300

    add logout

--- src/layout/Banner.js
+++ src/layout/Banner.js
@@ -1,3 +1,4 @@
+import { IconButton } from "@mui/material";
 import React, { useState } from "react";
 import {
   ExploreIcon,
@@ -10,6 +11,7 @@ import {
   TweetIcon,
 } from "../assets/icons";
 import { BannerLink } from "../components/BannerLink";
+import { useAuth } from "../firebase/auth";
 
 const bannerLinks = [
   {
@@ -38,6 +40,8 @@ const bannerLinks = [
   },
 ];
 const Banner = () => {
+  const { authUser, signOut } = useAuth();
+
   const [active, setActive] = useState("Home");
 
   const handleMenuItemClick = (name) => {
@@ -45,26 +49,31 @@ const Banner = () => {
   };
 
   return (
-    <header className="sticky top-0 w-20 h-screen flex-col px-2 justify-center">
-      <div className="p-3 w-12 h-12 my-1  text-primary-base rounded-full cursor-pointer mx-auto flex-col items-center justify-center  hover:bg-primary-lighter">
-        <TwitterIcon />
+    <header className="sticky top-0 w-20 h-screen flex flex-col px-2 justify-between">
+      <div className="flex flex-col justify-center">
+        <div className="p-3 w-12 h-12 my-1  text-primary-base rounded-full cursor-pointer mx-auto flex-col items-center justify-center  hover:bg-primary-lighter">
+          <TwitterIcon />
+        </div>
+        <nav className="w-12 mx-auto">
+          <ul>
+            {bannerLinks.map(({ name, icon }) => (
+              <BannerLink
+                key={name}
+                name={name}
+                Icon={icon}
+                active={active}
+                onMenuItemClick={handleMenuItemClick}
+              />
+            ))}
+          </ul>
+        </nav>
+        <button className="p-3 w-12 h-12 my-2 bg-primary-base text-white rounded-full cursor-pointer hover:bg-primary-dark mx-auto flex items-center justify-center shadow-lg transform transition-colors duration-200">
+          <TweetIcon />
+        </button>
       </div>
-      <nav className="w-12 mx-auto">
-        <ul>
-          {bannerLinks.map(({ name, icon }) => (
-            <BannerLink
-              key={name}
-              name={name}
-              Icon={icon}
-              active={active}
-              onMenuItemClick={handleMenuItemClick}
-            />
-          ))}
-        </ul>
-      </nav>
-      <button className="p-3 w-12 h-12 my-2 bg-primary-base text-white rounded-full cursor-pointer hover:bg-primary-dark mx-auto flex items-center justify-center shadow-lg transform transition-colors duration-200">
-        <TweetIcon />
-      </button>
+      <div className="w-12 p-3 mx-auto  bg-primary-base text-white rounded-full cursor-pointer hover:bg-primary-dark flex items-center justify-center shadow-lg transform transition-colors duration-200">
+        <button onClick={signOut}>Logout</button>
+      </div>
     </header>
   );
 };
