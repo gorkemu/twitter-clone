9aaceadf4ebc579718b3412d7e853731c0e9e610
gorkemu <gorkemunal88@gmail.com>
2023-02-25 04:46:34 +0300

    remove draft avatar, display name, username

--- src/components/FeedItem.js
+++ src/components/FeedItem.js
@@ -9,22 +9,15 @@ import {
 } from "../assets/icons";
 import ReactTimeAgo from "react-time-ago";
 
-const FeedItem = ({
-  avatar,
-  content,
-  image,
-  createdAt,
-  displayName,
-  username,
-}) => {
+const FeedItem = ({ content, imageUrl, createdAt }) => {
   return (
     <article className="flex space-x-3 border-b border-l-gray-lighter px-4 py-3 cursor-pointer hover:bg-gray-lightest">
-      <img src={avatar} alt="Profile" className="w-12 h-12 rounded-full" />
+      {/* <img src={avatar} alt="Profile" className="w-12 h-12 rounded-full" /> */}
       <div className="flex-1">
         <div className="flex items-center justify-between">
           <div className="flex items-center">
-            <h4 className="font-bold hover:underline">{displayName}</h4>
-            <span className="ml-2 text-gray-dark">{username}</span>
+            {/* <h4 className="font-bold hover:underline">{displayName}</h4>
+            <span className="ml-2 text-gray-dark">{username}</span> */}
             <div className="mx-2 bg-gray-dark h-1 w-1 border rounded-full" />
             <span className="text-gray-dark hover:underline">
               {createdAt && (
@@ -37,8 +30,12 @@ const FeedItem = ({
           </div>
         </div>
         <p className="">{content} </p>
-        {image && (
-          <img src={image} alt="Tweeted Pic" className="rounded-xl max-h-96" />
+        {imageUrl !== "" && (
+          <img
+            src={imageUrl}
+            alt="Tweeted Pic"
+            className="rounded-xl max-h-96"
+          />
         )}
         <ul className="flex justify-between mt-1 -ml-2 max-w-lg">
           <li className="group flex items-center space-x-1">
