6987f0a5cefa0dcbecc80b89df376404e217deba
gorkemu <gorkemunal88@gmail.com>
2023-03-02 01:32:15 +0300

    distribute navigate logic to login, register pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { useEffect } from "react";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import { useAuth } from "./firebase/auth";
 import Register from "./pages/Register";
+import Logout from "./pages/Logout";
 
 const App = () => {
-  const { authUser, isLoading } = useAuth();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!isLoading && authUser) {
-      navigate("/dashboard");
-    }
-  }, [authUser, isLoading, navigate]);
-
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/logout" element={<Logout />} />
       <Route path="/" element={<Home />} />
     </Routes>
   );
