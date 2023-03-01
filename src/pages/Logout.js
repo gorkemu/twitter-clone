ad8203a0ccd8d1906563ce6c283eb7cabccb4d51
gorkemu <gorkemunal88@gmail.com>
2023-03-02 02:21:09 +0300

    use navigate instead of Link for btn click

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { TwitterIcon } from "../assets/icons";
 import { useAuth } from "../firebase/auth";
 
