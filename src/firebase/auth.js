23689ee2bffca459847d555285117f5c09358fc1
gorkemu <gorkemunal88@gmail.com>
2023-03-02 12:45:54 +0300

    remove Google auth info that are not used

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -82,14 +82,9 @@ export const signUpWithGoogle = async () => {
   });
   signInWithPopup(auth, provider)
     .then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      const user = result.user;
+      GoogleAuthProvider.credentialFromResult(result);
     })
     .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.customData.email;
-      const credential = GoogleAuthProvider.credentialFromError(error);
+      alert(error.message);
     });
 };
